8f4e6c53ac80dfdf3f52c7ffc0627cb99adb12f0
xddbom <xddbom@gmail.com>
2025-01-09 15:17:00 +0200

    Added based logic for HTTP update to Ws.

---
+++ Handlers/handlers.go
@@ -0,0 +1,44 @@
+package Handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
+}
+
+type WebSocketHandler struct{}
+
+func(h *WebSocketHandler) WsHandle(c *gin.Context) {
+    conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+    if err != nil {
+        log.Println("Error upgrading connection:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update connection",})
+        return
+    }
+    defer conn.Close()
+
+	log.Println("New WebSocket connection established")
+
+    for {
+        _, msg, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Error reading message:", err)
+            break
+        }
+
+        log.Printf("Received message: %s", msg)
+
+        if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+            log.Println("Error sending message:", err)
+			break
+        }
+    }
+}
