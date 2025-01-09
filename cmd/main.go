cc539aefc14e425b8970f26851652ec64ff8018c
xddbom <xddbom@gmail.com>
2025-01-09 03:06:00 +0200

    Testing CI in action.

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-
+	"fmt"
 	"github.com/xddbom/rt-chat/routes"
 	"github.com/xddbom/rt-chat/db"
 )
@@ -12,6 +12,8 @@ func main() {
 	rdb := 	db.RedisSetup()	
 
 	routes.SetupRoutes(r, rdb)
+
+	fmt.Println("test ing workflow")
 
 	r.Run(":8080")
 }
